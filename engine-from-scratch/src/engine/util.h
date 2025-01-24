b556582d77a2926a9b1a533334a7fc1f773d7ad2
Ehsan Foadian <ehsanfoadian@gmail.com>
2025-01-24 16:01:28 +0300

    [Dylan Falconer] Point vs AABB Collision Detection | C Game + Engine From Scratch 06 https://youtu.be/EbAx7XrRP7s?feature=shared

--- engine-from-scratch/src/engine/util.h
+++ engine-from-scratch/src/engine/util.h
@@ -1,9 +1,17 @@
-#ifndef UTIL_H
-#define UTIL_H
+#pragma once
 
 #include <stdio.h>
 
 #define ERROR_EXIT(...) { fprintf(stderr, __VA_ARGS__); exit(1); }
 #define ERROR_RETURN(R, ...) { fprintf(stderr, __VA_ARGS__); return R; }
 
-#endif
+#define WHITE		(vec4){1,1,1,1}
+#define BLACK		(vec4){0,0,0,1}
+#define RED			(vec4){1,0,0,1}
+#define GREEN		(vec4){0,1,0,1}
+#define YELLOW		(vec4){1,1,0,1}
+#define CYAN		(vec4){0,1,1,1}
+#define MAGNETA		(vec4){1,0,1,1}
+#define ORANGE		(vec4){1,0.5,0,1}
+#define PURPLE		(vec4){0.5,0,1,1}
+#define TURQUISE	(vec4){0,1,0.5,1}
